4548d0cfd1c3bbc6419d56771becddc21b245c22
ada_aster <an.ada.poirier@gmail.com>
2026-01-07 11:12:46 -0500

    Add Core Injections for Vegetation

---
+++ src/main/java/net/dusty_dusty/cts_compats/core/CompatSlabsMap.java
@@ -0,0 +1,35 @@
+package net.dusty_dusty.cts_compats.core;
+
+import net.minecraft.world.level.block.Block;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompatSlabsMap {
+
+    public static Map<Block, Block > compatOnTopVegitationMap = new HashMap<>();
+
+    public static Map< Block, Block > compatSlabMap = new HashMap<>();
+
+    public static boolean mapVegitation( Block originalBlock, Block onTopBlock ) {
+        if ( compatOnTopVegitationMap.containsKey( originalBlock ) ||
+                compatOnTopVegitationMap.containsValue( onTopBlock )
+        ) {
+            return false;
+        }
+
+        compatOnTopVegitationMap.put( originalBlock, onTopBlock ) ;
+        return true;
+    }
+
+    public static boolean mapTerrainSlab( Block originalBlock, Block slabBlock ) {
+        if ( compatSlabMap.containsKey( originalBlock ) ||
+                compatSlabMap.containsValue( slabBlock )
+        ) {
+            return false;
+        }
+
+        compatSlabMap.put( originalBlock, slabBlock ) ;
+        return true;
+    }
+}
