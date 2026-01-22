dfe81b51ff79c38443cb27a96104411255fe65eb
ada_aster <an.ada.poirier@gmail.com>
2026-01-21 23:32:39 -0500

    Add on top item handling

--- src/main/java/net/dusty_dusty/cts_compats/core/AssignUtil.java
+++ src/main/java/net/dusty_dusty/cts_compats/core/AssignUtil.java
@@ -1,29 +1,36 @@
 package net.dusty_dusty.cts_compats.core;
 
 import net.countered.terrainslabs.block.ModSlabsMap;
+import net.countered.terrainslabs.callbacks.RegisterCallbacks;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class AssignUtil {
 
     public static void putOnTopVegetation( Block key, Block value ) {
-        String[] keyStrings = getBlockIdComponents( key );
-        String[] valueStrings = getBlockIdComponents( value );
+        String[] keyStrings = getIdComponents( key );
+        String[] valueStrings = getIdComponents( value );
         ModSlabsMap.putOnTopVegetationFromString( keyStrings[0], keyStrings[1], valueStrings[0], valueStrings[1] );
     }
     public static void putTerrainSlab( Block key, Block value ) {
-        String[] keyStrings = getBlockIdComponents( key );
-        String[] valueStrings = getBlockIdComponents( value );
+        String[] keyStrings = getIdComponents( key );
+        String[] valueStrings = getIdComponents( value );
         ModSlabsMap.putTerrainSlabFromString( keyStrings[0], keyStrings[1], valueStrings[0], valueStrings[1] );
     }
     public static void putBlockBelowReplacement( Block key, Block value ) {
-        String[] keyStrings = getBlockIdComponents( key );
-        String[] valueStrings = getBlockIdComponents( value );
+        String[] keyStrings = getIdComponents( key );
+        String[] valueStrings = getIdComponents( value );
         ModSlabsMap.putBlockBelowReplacementFromString( keyStrings[0], keyStrings[1], valueStrings[0], valueStrings[1] );
     }
     public static void putTopSlabReplacement( Block key, Block value ) {
-        String[] keyStrings = getBlockIdComponents( key );
-        String[] valueStrings = getBlockIdComponents( value );
+        String[] keyStrings = getIdComponents( key );
+        String[] valueStrings = getIdComponents( value );
         ModSlabsMap.putTopSlabReplacementFromString( keyStrings[0], keyStrings[1], valueStrings[0], valueStrings[1] );
+    }
+    public static void putVegetaitonOnTopItem( Item item, Block block ) {
+        String[] keyStrings = getIdComponents( item );
+        String[] valueStrings = getIdComponents( block );
+        RegisterCallbacks.putVegetaitonOnTopItemFromString( keyStrings[0], keyStrings[1], valueStrings[0], valueStrings[1] );
     }
 
 //    private static String getBlockModId( Block block ) {
@@ -32,8 +39,12 @@ public class AssignUtil {
 //    private static String getBlockName( Block block ) {
 //        return block.getDescriptionId().split("\\.")[2];
 //    }
-    private static String[] getBlockIdComponents( Block block ) {
+    private static String[] getIdComponents( Block block ) {
         String[] strs = block.getDescriptionId().split("\\.");
+        return new String[] { strs[1], strs[2] };
+    }
+    private static String[] getIdComponents( Item item ) {
+        String[] strs = item.getDescriptionId().split("\\.");
         return new String[] { strs[1], strs[2] };
     }
 
